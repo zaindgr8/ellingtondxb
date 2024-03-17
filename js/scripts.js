384578dd56f0ab6642a47a276eb2c0c0bc22d1b7
zaindgr8 <66069515+zaindgr8@users.noreply.github.com>
2024-03-17 16:34:19 +0400

    434

--- js/scripts.js
+++ js/scripts.js
@@ -547,7 +547,6 @@ $(window).on("load", function() {
                 success: function(data) {
                     var messageAlert = 'alert-' + data.type;
                     var messageText = data.message;
-
                     var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
                     if (messageAlert && messageText) {
                         $('#contact-form').find('.messages').html(alertBox);
